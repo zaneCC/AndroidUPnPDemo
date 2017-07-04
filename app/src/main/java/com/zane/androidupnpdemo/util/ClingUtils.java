40a10af1853d12b892a2ba2b339cf23e207f4349
zhouzhan <zhouzhan@mgtv.com>
2017-07-04 10:54:05 +0800

    Cling 工具类

---
+++ app/src/main/java/com/zane/androidupnpdemo/util/ClingUtils.java
@@ -0,0 +1,53 @@
+package com.zane.androidupnpdemo.util;
+
+import android.support.annotation.Nullable;
+
+import com.zane.androidupnpdemo.entity.IControlPoint;
+import com.zane.androidupnpdemo.entity.IDevice;
+import com.zane.androidupnpdemo.service.manager.ClingUpnpServiceManager;
+
+import org.fourthline.cling.controlpoint.ControlPoint;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.Service;
+import org.fourthline.cling.model.types.ServiceType;
+
+/**
+ * 说明：Cling 库使用工具类
+ * 作者：zhouzhan
+ * 日期：17/7/4 10:27
+ */
+
+public class ClingUtils {
+
+
+    /**
+     * 通过 ServiceType 获取已选择设备的服务
+     *
+     * @param serviceType   服务类型
+     * @return  服务
+     */
+    @Nullable
+    public static Service findServiceFromSelectedDevice(ServiceType serviceType){
+        IDevice selectedDevice = ClingUpnpServiceManager.getInstance().getSelectedDevice();
+        if (Utils.isNull(selectedDevice)){
+            return null;
+        }
+
+        Device device = (Device) selectedDevice.getDevice();
+        return device.findService(serviceType);
+    }
+
+    /**
+     * 获取控制点
+     *
+     * @return  控制点
+     */
+    @Nullable
+    public static ControlPoint getControlPoint(){
+        IControlPoint controlPoint = ClingUpnpServiceManager.getInstance().getControlPoint();
+        if (Utils.isNull(controlPoint))
+            return null;
+
+        return  (ControlPoint) controlPoint.getControlPoint();
+    }
+}
